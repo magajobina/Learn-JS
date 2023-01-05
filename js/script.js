843d454f81b75a626b5b5876e0b91dbb8ca7d52a
Alik <example@gmail.com>
2023-01-05 20:59:20 +0300

    практика 2

--- js/script.js
+++ js/script.js
@@ -1,44 +1,8 @@
-/* Задание на урок:
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
-'use strict';
-
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
-};
-
-const film1 = prompt('Один из последних просмотренных фильмов?', ''),
-      quest1 = prompt('На сколько оцените его?', ''),
-      film2 = prompt('Один из последних просмотренных фильмов?', ''),
-      quest2 = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[film1] = quest1;
-personalMovieDB.movies[film2] = quest2;
-
-console.log(personalMovieDB);
+if (hamburger && cola || fries === 3 && nuggets) {
+  console.log('Done!');
+}
