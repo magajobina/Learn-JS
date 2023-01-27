3e4e393bc552045fb58a4f635cd8be07d4c9e271
Magajobina <magajobina@gmail.com>
2023-01-27 21:39:57 +0300

    переключение табов на инлайн стилях

--- js/script.js
+++ js/script.js
@@ -1,95 +1,53 @@
-/* Задания на урок:
+window.addEventListener('DOMContentLoaded', () => { 
 
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+  const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
 
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+  function hideTabContent() {
+    tabsContent.forEach(item => { //выбираем все табы
+      item.style.display = 'none'; //скрываем все табы
+    });
 
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+    tabs.forEach(item => { //выбираем все кнопки переключения табов
+      item.classList.remove('tabheader__item_active'); //удаляем со всех класс активности
+    });
+  }
+  
+  function showTabContent(i = 0) {
+    tabsContent[i].style.display = 'block';
+    tabs[i].classList.add('tabheader__item_active');
+  }
+  
+  hideTabContent();
+  showTabContent();
 
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
-"Добавляем любимый фильм"
+  tabsParent.addEventListener('click', (event) => {
+    const target = event.target; //это мы обращаемся к элементу на который кликнули, и помещаем в переменную для краткости записи
 
-5) Фильмы должны быть отсортированы по алфавиту */
-
-
-'use strict';
-document.addEventListener('DOMContentLoaded', () => {  //теперь наша колбек функция (т.е. весь код) будет работать только после загрузки DOM дерева
-
-
-
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Одержимостььтьтьтьтьтьтьтьтьтьтьтьт",
-        "Скотт Пилигрим против..."
-    ]
-};
-
-const advBanners = document.querySelectorAll('.promo__adv img'),
-    genre = document.querySelector('.promo__genre'),
-    replaceBG = document.querySelector('.promo__bg'),
-    movieList = document.querySelector('.promo__interactive-list');
-let addForm = document.querySelector('form.add'),                         //получаем всё что в теге form
-    addInput = addForm.querySelector('.adding__input'),
-    checkBox = addForm.querySelector('[type="checkbox"]');
-
-
-addForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (addInput.value) { //смотрим чтоб инпут вообще пустым не был. Если хоть чёт есть, тогда выполняем
-        movieDB.movies.push(addInput.value);
-        createMovieList();
-        addInput.value = '';
-        if (checkBox.checked) {
-            console.log("Добавляем любимый фильм");
+    if (target && target.classList.contains('tabheader__item')) { //условие если мы кликнули куда и было надо, т.е. на эл с указанным классом, то...
+      tabs.forEach((item, i) => { //перебираем все кнопки переключения табов
+        if (item == target) { //если перебираемый элемент(item) совпадает с тем что мы нажали(target)
+          hideTabContent(); //то мы сначала скрываем все табы и удаляем все классы активности для кнопок
+          showTabContent(i); //а затем показываем таб с порядковым номером i, и вешаем класс на кнопку под тем же порядковым номером, и оно
+                            //идеально совпадет потому что кол-во кнопок такое же как и кол-во табов
         }
+      });
+      
     }
-});
+  });
 
-createMovieList();
 
-function createMovieList () {
-    movieList.innerHTML = '';  //удаляем весь прошлый список элементов со страницы и даьше в этом цикле занимаемся отрисовкой нового списка
-    movieDB.movies.sort();  // сортирует по алфавиту эл массива movies
-    movieDB.movies.forEach((filmName, i) => {  //Тут мы будем добавлять HTML структуры в нужное место
-        function checkTheLength() { //эта функция проверяет длину названия фильма, если больше 21 символа то обрезает до 21го и добавляет точки в конце
-            let result = filmName; //filmName берётся из внешней функции forEach
-            if (filmName.length > 21) {
-                result = filmName.substring(0, 21) + '...';
-            }
-            return result; //возвращаем результат (обработанную строку) в место вызова функции (там где HTML код добавляется на страницу)
-        }
-        //Тут мы добавляем HTML структуры в нужное место
-        movieList.innerHTML += `
-        <li class="promo__interactive-item">${i+1}. ${checkTheLength()}
-        <div class="delete"></div>
-        </li>
-        `; 
-    });
-    document.querySelectorAll('.delete').forEach((btn, i) => { //навешиваем обработчик событий на каждую кнопку .delete 
-        btn.addEventListener('click', () => {
-            btn.parentElement.remove(); //удаляем родителя
-            movieDB.movies.splice(i, 1); //обрезаем массив
-            createMovieList(); //заново отрисовываем и сортируем
-        });
-    });
-}
 
-advBanners.forEach(item => {
-    item.remove();  //удаляем рекламу
-});
 
-genre.textContent = 'ДРАМА'; //заменяем текст внутри элемента
 
-// replaceBG.style = 'background:url("../img/bg.jpg") center center/cover no-repeat; background-position: top;'; //это как я сделал
-replaceBG.style.backgroundImage = 'url("img/bg.jpg")'; //это как сделал преподаватель
+
+
+
+  // console.log(tabs[0].classList);
+
+
+
 
 
 
